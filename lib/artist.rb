fbcfcbb3f84c3617afce99ab9b7fa7e1359476f8
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-25 01:40:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,21 @@
 class Artist
   attr_accessor :name
+  @@all = []
+  def initialize(name)
+    self.name = name
+  end
+  def self.all
+    @@all
+  end
+  def self.destory_all
+    @@all.clear
+  end
+  def save
+    @@all << self
+  end
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
 end
